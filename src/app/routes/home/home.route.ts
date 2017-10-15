493b8538f89a822bee8d2f7df258a18cd138c2c6
shakedown-street <pricejordan@outlook.com>
2017-10-15 04:33:07 -0600

    Start getting sceditor ready.

--- src/app/routes/home/home.route.ts
+++ src/app/routes/home/home.route.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { ModalService, ConfirmModalComponent } from '../../modules/modals';
+
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-route-home',
   templateUrl: './home.route.html',
   styleUrls: ['./home.route.scss']
 })
-export class HomeRouteComponent {
+export class HomeRouteComponent implements AfterViewInit {
 
   constructor(
     private modals: ModalService,
   ) { }
+
+  public ngAfterViewInit() {
+    $('.post-editor').sceditor({
+      plugins: 'bbcode',
+      style: '../node_modules/sceditor/minified/jquery.sceditor.default.min.css'
+    });
+  }
 
   public openModal() {
     this.modals.open(ConfirmModalComponent, {
